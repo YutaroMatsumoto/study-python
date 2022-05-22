37bd6e5fa7c2550af53834b9e09ee90b6af16f70
YutaroMatsumoto <spurs03170629@gmail.com>
2022-05-22 22:38:00 +0900

    第２回講義

---
+++ 2nd_print.py
@@ -0,0 +1,7 @@
+# sepでコメントの区切り
+# endは最後
+print('Hi', 'Mike', sep=',', end='\n')
+print('Hi', 'Mike', sep=',', end='\n')
+
+print('Hi', 'Mike', sep=',', end='')
+print('Hi', 'Mike', sep='a', end='')

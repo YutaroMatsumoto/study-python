ae11de709aa3af5e5373f5c16a0d71ec65d4048c
YutaroMatsumoto <spurs03170629@gmail.com>
2022-05-24 23:56:37 +0900

    第１０回講義

---
+++ 10th_list3.py
@@ -0,0 +1,25 @@
+# 講義タイトル：リストのメソッド
+r = [1, 2, 3, 4, 5, 1, 2, 3,]
+print(r.index(3))
+print(r.index(3, 3))
+
+print(r.count(3))
+
+r.sort()
+print(r)
+r.sort(reverse=True)
+print(r)
+r.reverse()
+print(r)
+r.reverse()
+print(r)
+
+s = 'My name is Mike.'
+print(s)
+to_split = s.split(' ')
+print(to_split)
+
+x = ' ######## '.join(to_split)
+print(x)
+
+# print(help(list))

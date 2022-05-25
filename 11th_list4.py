11303d7bca6f88fdb4dc3295eab0b57144334c2a
YutaroMatsumoto <spurs03170629@gmail.com>
2022-05-25 23:57:36 +0900

    第１１回講義

---
+++ 11th_list4.py
@@ -0,0 +1,38 @@
+# 講義タイトル：リストのコピー
+
+# 参照渡し
+i = [1, 2, 3, 4, 5]
+j = i
+j[0] = 100
+print('j = ', j)
+print('i = ', i)
+
+print('########################')
+
+# 値渡し
+x = [1, 2, 3, 4, 5]
+y = x.copy() # y = x[:]も同じ
+y[0] = 100
+print('y = ', y)
+print('x = ', x)
+
+print('########################')
+
+# 値渡し
+X = 20
+Y = X
+Y = 5
+print(id(X))
+print(id(Y))
+print(Y)
+print(X)
+
+print('########################')
+
+X = ['a', 'b']
+Y = X
+Y[0] = 'p'
+print(id(X))
+print(id(Y))
+print(Y)
+print(X)

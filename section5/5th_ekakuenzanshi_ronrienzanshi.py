3cc704903b031c79e59df8e7124bd2a2421b0588
YutaroMatsumoto <spurs03170629@gmail.com>
2022-06-01 23:24:18 +0900

    section2-5

---
+++ section5/5th_ekakuenzanshi_ronrienzanshi.py
@@ -0,0 +1,22 @@
+# 講義タイトル：比較演算子と論理演算子
+a = 1
+b = 1
+
+
+print(a == b)
+
+print(a != b)
+
+print(a < b)
+
+print(a > b)
+
+print(a <= b)
+
+print(a >= b)
+
+if a > 0 and b > 0:
+    print('a and b are positive')
+
+if a > 0 or b > 0:
+    print('a or b are positive')

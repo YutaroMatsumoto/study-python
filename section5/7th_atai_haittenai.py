263b44eb4fea065c62c8aeefb4d4ec2edba29849
YutaroMatsumoto <spurs03170629@gmail.com>
2022-06-02 22:19:29 +0900

    section5-7

---
+++ section5/7th_atai_haittenai.py
@@ -0,0 +1,11 @@
+# 講義タイトル：値が入っていない判定をするテクニック
+# is_ok = True
+# 文字列が空だとfalse
+# 配列も空だとfalse
+# False, 0, 0.0, '', [], (), {}, set()
+is_ok = []
+
+if is_ok:
+    print('OK!')
+else:
+    print('No!')
